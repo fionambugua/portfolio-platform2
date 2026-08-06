b1de4a1d8167b4e8ea31c2dd43982f0bc6433432
Fiona <fionambuguanjeri@gmail.com>
2026-08-06 21:51:19 +0300

    Refactor App component to implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
-  
-
   return (
-    <div>
-      <h1>Portfolio </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project/:id" element={<ProjectDetails />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
