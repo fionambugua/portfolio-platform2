63ab38a6cbe7ea9e92524ef1f8195ea96b63f1a6
Fiona <fionambuguanjeri@gmail.com>
2026-08-06 22:00:52 +0300

    Edited Header component UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <h2>MyPortfolio</h2>
+      </div>
+
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#add-project">Add Project</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
