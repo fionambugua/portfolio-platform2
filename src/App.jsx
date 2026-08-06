0ffba9c3684df0cf8f9f4da10270a085fbfbff29
Fiona <fionambuguanjeri@gmail.com>
2026-08-06 23:49:45 +0300

    Adding App states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./pages/Home";
 import ProjectDetails from "./pages/ProjectDetails";
 
+import projectData from "./data/projects";
+
 function App() {
+  const [projects, setProjects] = useState(projectData);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/project/:id" element={<ProjectDetails />} />
+      <Route
+        path="/"
+        element={<Home projects={projects} setProjects={setProjects} />}
+      />
+
+      <Route
+        path="/project/:id"
+        element={<ProjectDetails projects={projects} />}
+      />
     </Routes>
   );
 }
